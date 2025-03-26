6394cdecb8f4de1743adb4a0a8d3760194ecdad9
WenYaFei <1506825038@qq.com>
2025-03-26 17:51:20 +0800

    部分框架修改

--- colly/main/main.go
+++ colly/main/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+)
 
+// 定义 JSON 对应的结构体
+type Book struct {
+	Title  string `json:"title"`
+	Author string `json:"author"`
+	Price  int    `json:"price"`
+}
+
+func main() {
+	jsonStr := `{"title": "Golang入门", "author": "张三", "price": 100}`
+
+	// 创建结构体变量
+	var book Book
+
+	// 解析 JSON
+	err := json.Unmarshal([]byte(jsonStr), &book)
+	if err != nil {
+		fmt.Println("JSON 解析错误:", err)
+		return
+	}
+
+	// 输出解析后的数据
+	fmt.Printf("书名: %s, 作者: %s, 价格: %d\n", book.Title, book.Author, book.Price)
 }
